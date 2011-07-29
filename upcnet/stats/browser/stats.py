e8be257b2bbf243440fadcc39dbe486d1104f98c
ariel.von.barnekow <ariel.von.barnekow@6deaf63b-a510-49cf-aad4-8c651776ecce>
2011-07-29 10:08:55 +0000

    upcnet.stats - canvi del nom de la vista per gwc_stats_info, eliminació de l'instalador ja que no calia i revisió del funcionament
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upcnet.stats@2245 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upcnet/stats/browser/stats.py
+++ upcnet/stats/browser/stats.py
@@ -6,7 +6,7 @@ from DateTime import DateTime
 
 from zope.app.component.hooks import getSite
 
-
+from plone.app.controlpanel.mail import IMailSchema
 
 def userLoginHandler (event):
     """
@@ -14,12 +14,11 @@ def userLoginHandler (event):
         de l'ultim login.
     """
     site = getSite()
-
     properties = getToolByName(site,'portal_properties').site_properties
     if properties.hasProperty('last_login') == 0 :
-        properties.manage_addProperty('last_login',DateTime(),'date')
+        properties.manage_addProperty('last_login',str(DateTime().ISO()),'string')
     else:
-        properties.manage_changeProperties({'last_login':DateTime().ISO()})
+        properties.manage_changeProperties(last_login=str(DateTime().ISO()))
 
 class StatsView(BrowserView):
     """
@@ -33,29 +32,40 @@ class StatsView(BrowserView):
         self.context = context
         self.request = request
         self.properties = getToolByName(context,'portal_properties').site_properties
-        if self.properties.hasProperty('last_login') == 0 :
-            self.properties.manage_addProperty('last_login','','string')
-
+               
+    def getContactEmail(self):
+        """
+            Returns the contact email
+        """
+        portal = getToolByName(self,'portal_url').getPortalObject()
+        mail = IMailSchema(portal)         
+        return mail.email_from_address
+        
     def getInactivitat(self):
         """
-            Retunrs the days from the last login.
+            Returns the days from the last login.
         """
-        isodate = self.properties.last_login
-        if isodate == '' :
-            days = -1
-        else:
+        days = -1
+        if self.properties.hasProperty('last_login') == 1 :
+            isodate = self.properties.last_login
             dt = DateTime(isodate)
             last_access = dt.timeTime()
             minutes = int((DateTime().timeTime() - last_access)/60.0)
             days = int(minutes/86400.0)
+
         return  days
 
+    def getTitol(self):
+        """
+        Retorna el titol del lloc
+        """
+        titol = self.context.Title()
+        return titol
 
     def getSize(self):
         """
             Returns the size of the plone instance
         """
-
         size = 0
         portal_catalog = getToolByName (self.context, 'portal_catalog')
         type_filter = ['Folder', 'Large Plone Folder']
@@ -74,6 +84,7 @@ class StatsView(BrowserView):
         """
             Returns de size in human readable format
         """
+        # No s'utilitza en cap lloc, pero pot ser interesant
         for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
             if num < 1024.0:
                 return "%3.1f %s" % (num, x)
